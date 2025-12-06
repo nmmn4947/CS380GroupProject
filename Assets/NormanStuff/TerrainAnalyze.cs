72bac976dfabbf29e29b3affda63a786a898d6b9
nmmn4947 <nmmn4947@gmail.com>
2025-12-05 23:31:00 -0800

    BingBong

--- Assets/NormanStuff/TerrainAnalyze.cs
+++ Assets/NormanStuff/TerrainAnalyze.cs
@@ -3,11 +3,12 @@ using UnityEngine.AI;
 
 public class TerrainAnalyze : MonoBehaviour
 {
+    NavMeshTriangulation triangulation;
     public NavMeshData nmd;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        NavMesh.CalculateTriangulation();
     }
 
     // Update is called once per frame
